513a9d446bf0a5ec2889b5b38368c350bbdb0236
Denis Sanchez <dsanchez@campus-eni.fr>
2025-09-30 18:52:17 +0200

    test br

--- quiz.js
+++ quiz.js
@@ -54,7 +54,7 @@ const questionsData = {
                 "Une base de données est un système organisé qui permet de stocker, gérer et récupérer des données de manière efficace"
             ],
             correct: 3,
-            explanation: "Une base de données est un système organisé qui permet de stocker, gérer et récupérer des données de manière efficace. Elle est conçue pour faciliter l'accès et la manipulation des informations, garantissant ainsi leur intégrité et leur sécurité. Les bases de données peuvent être classées en plusieurs types, notamment les bases de données relationnelles, qui utilisent des tables pour structurer les données, et les bases de données NoSQL, qui offrent une flexibilité accrue pour le stockage de données non structurées."
+            explanation: "Une base de données est un système organisé qui permet de stocker, gérer et récupérer des données de manière efficace. <br>Elle est conçue pour faciliter l'accès et la manipulation des informations, garantissant ainsi leur intégrité et leur sécurité. Les bases de données peuvent être classées en plusieurs types, notamment les bases de données relationnelles, qui utilisent des tables pour structurer les données, et les bases de données NoSQL, qui offrent une flexibilité accrue pour le stockage de données non structurées."
         },
         {
             question: "Qu'est-ce qu'un SGBD ?",
